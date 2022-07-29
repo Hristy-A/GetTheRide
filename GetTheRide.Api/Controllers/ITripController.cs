c491a6b5b8879848f2fb54ada8424a7309b2e43b
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2022-07-29 16:05:50 +0300

    Describe TripController

---
+++ GetTheRide.Api/Controllers/ITripController.cs
@@ -0,0 +1,13 @@
+﻿using GetTheRide.BL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetTheRide.Api.Controllers
+{
+    public interface ITripController
+    {
+        Task<IActionResult> GetAsync(Domain.TripState state);
+        Task<IActionResult> CreateAsync(Trip trip);
+        Task<IActionResult> DeleteAsync(int tripId);
+        Task<IActionResult> ManagePassengerAsync(int tripId, Passenger passenger, bool assign);
+    }
+}
--- GetTheRide.Api/Controllers/TripController.cs
+++ GetTheRide.Api/Controllers/TripController.cs
@@ -1,68 +1,99 @@
 using AutoMapper;
+using GetTheRide.BL;
 using GetTheRide.DataAccess.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Trip = GetTheRide.BL.Trip;
 
 namespace GetTheRide.Api.Controllers
 {
+    /*
+- /trips
+get: Get all Trips
+  params: state=TripState
+post: Create new Trip
+- /{id}
+  post: Assign/remove a passenger to/from the Trip
+  delete: Cancel Trip
+
+- /drivers
+post: Create driver's account
+put: Edit driver's account
+delete: Delete driver's account
+- /{id}
+  get: Get driver's account info
+
+- /passengers
+post: Create passenger's account
+put: Edit passenger's account
+delete: Delete passenger's account
+- /{id}
+  get: Get passenger's account info
+   */
+
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class TripController : ControllerBase
+    public class TripController : ControllerBase, ITripController
     {
         private readonly IMapper _mapper;
         private readonly ILogger<TripController> _logger;
         private readonly GetTheRideDbContext _dbContext;
 
         public TripController(
-            ILogger<TripController> logger, 
-            IMapper mapper, 
+            ILogger<TripController> logger,
+            IMapper mapper,
             GetTheRideDbContext dbContext)
         {
             _logger = logger;
             _mapper = mapper;
             _dbContext = dbContext;
-
-            // For single entity
-            //_mapper.Map<Trip>(object)
-
-            // For colelctions
-            //_mapper.ProjectTo<Trip>(collection)
         }
 
-        //[HttpPost]
-        //public ActionResult<Driver> AddDriver(Driver driver, GetTheRideDbContext context)
-        //{
-        //    // if login exists checking
-
-        //    var user = _mapper.Map<User>(driver);
-
-        //    context.Users.Add(_mapper.Map<User>(driver));
-        //}
-
-        [HttpGet(Name = "GetTrips")]
-        public IEnumerable<Trip> Get()
+        /// <summary>
+        /// Get all trips in given state.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] Domain.TripState state)
         {
-            var dbTrip = new Domain.Trip()
-            {
-                Id = 1,
-                AvailableSeats = 4,
-                State = Domain.TripState.Open,
-                Driver = new Domain.User()
-                {
-                    Id = 1,
-                    FirstName = "a",
-                    LastName = "b",
-                    Vehicle = new Domain.Vehicle()
-                    {
-                        Id = 1,
-                        Seats = 4,
-                        Name = "car",
-                    }
-                },
-            };
+            throw new NotImplementedException();
+        }
 
-            var trip = _mapper.Map<Trip>(dbTrip);
-            return new List<Trip>() { trip };
+        /// <summary>
+        /// Create trip.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Cancel trip.
+        /// </summary>
+        [HttpDelete("{tripId}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int tripId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Assign/remove passenger to/from the trip.
+        /// </summary>
+        [HttpPost("{tripId}")]
+        public async Task<IActionResult> ManagePassengerAsync([FromRoute] int tripId, [FromBody] Passenger passenger, [FromQuery] bool assign)
+        {
+            if (assign)
+                return await AddPassenger(tripId, passenger);
+            else
+                return await RemovePassenger(tripId, passenger);
+        }
+
+        private async Task<IActionResult> AddPassenger(int tripId, Passenger passenger)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<IActionResult> RemovePassenger(int tripId, Passenger passenger)
+        {
+            throw new NotImplementedException();
         }
     }
 }
